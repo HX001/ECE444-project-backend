d6fe03d20774d30d6fc670ee97f374d504570c94
HX001 <hongxu2015@gmail.com>
2020-11-18 15:27:08 -0500

    Change db import source

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask_mongoengine import MongoEngine
 # local packages
 from flask import app, jsonify
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_mongoengine import MongoEngine
+from init import db
 from flask_cors import CORS
 import routes_manage
 
 CONN_STR = 'mongodb+srv://yitong:password3666@test.zfblj.gcp.mongodb.net/test?retryWrites=true&w=majority'
-db = MongoEngine()
+
 
 app = Flask(__name__)
 CORS(app)
